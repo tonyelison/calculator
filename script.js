a105bcefd4771ee9b04f1b6ebd10c1776cfc32d2
Anthony Elison <tonyelison37@gmail.com>
2023-02-23 12:47:59 -0700

    Add generic operate function

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+const OPERATIONS = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide,
+};
+
+const operate = (operator, num1, num2) => OPERATIONS[operator](num1, num2);
